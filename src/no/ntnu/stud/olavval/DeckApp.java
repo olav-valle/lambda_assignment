ed982ea968bb8a48399877fb3ac8b4c8868159ac
Olav Valle <olavval@stud.ntnu.no>
2020-01-16 16:00:59 +0100

    finishes with a-g, starts h, i.

--- src/no/ntnu/stud/olavval/DeckApp.java
+++ src/no/ntnu/stud/olavval/DeckApp.java
@@ -29,7 +29,7 @@ public class DeckApp {
         hand = deck.assign(10);
 
         // prints all cards in hand
-        System.out.println(String.format("Printing all %s cards in hand:", hand.size()));
+        System.out.print(String.format("Printing all %s cards in hand: ", hand.size()));
         hand.stream()
                 .map(Card::getDetails)
                 .sorted() //TODO fix sorting by numeric value. x1 x11 x12 x10 x2 etc..
@@ -42,10 +42,16 @@ public class DeckApp {
         //1d) filter and print specific suit from hand
         //TODO refactor to separate method and make calls for each suit
 
-        System.out.println("Printing Clubs suit cards: ");
-        hand.stream().filter(Card -> (Card.getSuit() == 'C'))
+        System.out.print("Printing all Spades suit cards: ");
+        hand.stream()
+                .filter(Card -> (Card.getSuit() == 'S'))
                 .map(Card::getDetails)
-                .forEach(System.out::println);
+                .sorted() //TODO fix sorting by numeric value. x1 x11 x12 x10 x2 etc..
+                .forEachOrdered(
+                        det -> Stream
+                                .concat( Stream.of(det), Stream.of(", ") ) //separates cards with ", "
+                                .forEachOrdered(System.out::print) );
+        System.out.println(""); //line break
 
 
         // 1e) filters heart cards from hand into new set
@@ -53,13 +59,31 @@ public class DeckApp {
                 .collect(Collectors.toCollection(HashSet::new));
 
         //print heart cards
-        System.out.println( String.format( "Printing all %s heart cards:", cardsOfHeart.size() ) );
+        System.out.print( String.format( "Printing all %s heart cards: ", cardsOfHeart.size() ) );
         cardsOfHeart.stream()
                 .map(Card::getDetails) //map to card details
-                .forEach(System.out::println);
+                .sorted() //TODO fix sorting by numeric value. x1 x11 x12 x10 x2 etc..
+                .forEachOrdered(
+                        det -> Stream
+                                .concat( Stream.of(det), Stream.of(", ") ) //separates cards with ", "
+                                .forEachOrdered(System.out::print)
+                );
+        System.out.println("");
 
+        //1f) stream map cards to stream of suit chars
+        System.out.print("Printing only suit value of cards in hand: ");
+        hand.stream().map(Card::getSuit).sorted().forEachOrdered(System.out::print);
+        System.out.println("");
 
-
+        //1g) stream reduce card values to sum of values in hand
+        System.out.println(
+                String.format("The sum of the face values on hand is %s",
+                        hand.stream()
+                        .map(Card::getFace)
+                        .reduce(0, Integer::sum)
+                )
+        );
 
     }
+
 }
